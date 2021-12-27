3e042171e93cb94d5fbf99a113061cd8bf99aa40
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2021-12-27 20:50:08 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 #include "freertos/task.h"
 #include "sdkconfig.h"
 #include "esp_core_dump.h"
+#include "driver/uart.h"
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
+
+void crash_me_hard();
 
 void app_main()
 {
@@ -11,9 +16,13 @@ void app_main()
 	esp_core_dump_init();
 	esp_core_dump_summary_t *summary = malloc(sizeof(esp_core_dump_summary_t));
 	if (summary) {
+		esp_log_level_set("esp_core_dump_elf", ESP_LOG_VERBOSE); // so that func below prints stuff.. but doesn't actually work, have to set logging level globally through menuconfig
+		printf("Retrieving core dump summary..\n");
 		esp_err_t err = esp_core_dump_get_summary(summary);	
 		if (err == ESP_OK) {
-			printf("Getting core dump summary ok.");
+			//get summary function already pints stuff
+			printf("Getting core dump summary ok.\n");
+			//todo: do something with dump summary
 		} else {
 			printf("Getting core dump summary not ok. Error: %d\n", (int) err);
 			printf("Probably no coredump present yet.\n");
@@ -21,9 +30,19 @@ void app_main()
 		}		
 		free(summary);
 	}
+	fflush(stdout);
 	
-    while(1) {
+	for(int i=0; i <= 3; i++) {
+		printf("Crashing in %d seconds..\n", 3 - i);
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		printf("Doing nothing...\n");
-    }
+	}
+	printf("Crashing..\n");
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+	crash_me_hard();
+}
+
+void crash_me_hard() {
+	//provoke crash through writing to a nullpointer
+	volatile uint32_t* aPtr = (uint32_t*) 0x00000000;
+	*aPtr = 0x1234567; //goodnight
 }
